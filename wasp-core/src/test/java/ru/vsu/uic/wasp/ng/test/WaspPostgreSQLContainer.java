cf7ec89511c10949e8787eea037333aeb6f5d628
Evgeny Ryzhkov <evgeny.ryzhkov@gmail.com>
2024-08-01 16:43:46 +0300

    #5 Configure unit tests with Postgresql instance using Testcontainers
    Added the customized class for the PostgreSQL container for unit-tests. Now unit tests are run smoothly.

---
+++ wasp-core/src/test/java/ru/vsu/uic/wasp/ng/test/WaspPostgreSQLContainer.java
@@ -0,0 +1,34 @@
+package ru.vsu.uic.wasp.ng.test;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class WaspPostgreSQLContainer extends PostgreSQLContainer<WaspPostgreSQLContainer> {
+
+    private static final String IMAGE_VERSION = "postgres:16-alpine";
+    private static WaspPostgreSQLContainer container;
+
+    private WaspPostgreSQLContainer() {
+        super(IMAGE_VERSION);
+    }
+
+    public static WaspPostgreSQLContainer getInstance() {
+        if (container == null) {
+            // The database name, database username and the password will be created automatically in the container.
+            // The information will be passed to Spring configuration via the @ServiceConnection annotation
+            // in the test classes!
+            container = new WaspPostgreSQLContainer();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        // do nothing to not spin up a new docker container.
+        // JVM handles shutdown at the end of test.
+    }
+}
