e7a8fbb2cdc91170f09230e1626f8b503973a296
Evgeny Ryzhkov <evgeny.ryzhkov@gmail.com>
2024-08-17 10:26:26 +0300

    #50 UserRepository generates 3 queries to load a user
    Used EntityGraph to fetch account status and authentication type in a single query.

--- wasp-core/src/main/java/ru/vsu/uic/wasp/ng/core/dao/repository/UserRepository.java
+++ wasp-core/src/main/java/ru/vsu/uic/wasp/ng/core/dao/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package ru.vsu.uic.wasp.ng.core.dao.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.vsu.uic.wasp.ng.core.dao.entity.User;
 
@@ -7,6 +9,9 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    @EntityGraph(
+            value = "user-status-authtype-graph",
+            type = EntityGraphType.FETCH)
     User findByLogin(String login);
 
 }
